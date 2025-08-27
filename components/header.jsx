620693806b4d939c06f03f33c01e19bbb87d448b
Tuguldur Batbayar <Tuguldur09252@gmail.com>
2025-08-27 15:55:36 +0800

    Initial Tailwind CSS project setup

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+import { CiBank } from "react-icons/ci";
+
+const Header = () => {
+  return (
+    <header class="flex w-24">
+      <CiBank size="24" alt="Banking icon" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/main-content-area.jsx
@@ -0,0 +1,5 @@
+const MainContentArea = () => {
+  return <div></div>;
+};
+
+export default MainContentArea;
